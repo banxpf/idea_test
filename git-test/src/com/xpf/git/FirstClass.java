bad2f596570026048ac89a1879f9a13d12abb266
ban <415047736@qq.com>
2019-07-16 19:36:30 +0800

    本地工程添加到git

---
+++ git-test/src/com/xpf/git/FirstClass.java
@@ -0,0 +1,26 @@
+package com.xpf.git;
+
+import javax.sound.midi.SoundbankResource;
+
+public class FirstClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
